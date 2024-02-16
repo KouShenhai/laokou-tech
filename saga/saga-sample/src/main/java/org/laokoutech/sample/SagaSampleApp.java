8bf4884782516e8cd3eec9079531da187c169553
koushenhai <2413176044@qq.com>
2024-02-16 14:48:11 +0800

    11

---
+++ saga/saga-sample/src/main/java/org/laokoutech/sample/SagaSampleApp.java
@@ -0,0 +1,13 @@
+package org.laokoutech.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaSampleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaSampleApp.class, args);
+    }
+
+}
---
+++ saga/saga-sample/src/test/java/org/laokoutech/sample/SagaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laokoutech.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
